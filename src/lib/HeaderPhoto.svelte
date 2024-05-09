386c5e009ef71a9df1324509262c9d21e4477cee
justuskilianwolff <justus.wolff@web.de>
2024-05-09 21:21:17 +0200

    changed size

---
+++ src/lib/HeaderPhoto.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import img from '$lib/assets/example.jpg';
+</script>
+
+<div class="flex justify-center items-center h-48 w-48 rounded-full float-start">
+	<img class="rounded-full h-full w-full object-cover" src={img} alt="Headshot" />
+</div>
