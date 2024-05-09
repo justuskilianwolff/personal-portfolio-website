4bf335937feaa783e5a8b2c1276f3c2e825f93db
justuskilianwolff <justus.wolff@web.de>
2024-05-09 23:14:23 +0200

    card implementation for website

---
+++ src/lib/Card.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import * as Card from '$lib/components/ui/card';
+	export let title: string;
+	export let description: string;
+	export let content: string;
+	export let footer: string;
+
+	function getRandomNumber(min: number, max: number): number {
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+
+	let width = getRandomNumber(20, 25);
+	let height = getRandomNumber(15, 18);
+	console.log(width, height);
+	//max-w-80 min-w-56
+</script>
+
+<Card.Root style={`width: ${width}rem; height: ${height}rem`} class="bg-second">
+	<Card.Header>
+		<Card.Title>{title}</Card.Title>
+		<Card.Description>{description}</Card.Description>
+	</Card.Header>
+	<Card.Content>
+		<p>{content}</p>
+	</Card.Content>
+	<Card.Footer>
+		<p>{footer}</p>
+	</Card.Footer>
+</Card.Root>
