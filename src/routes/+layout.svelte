6001012dd9dd927d80c639ea49d3e2d917710778
justuskilianwolff <justus.wolff@web.de>
2024-05-09 21:21:27 +0200

    layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,6 @@
 	import '../app.css';
 </script>
 
-<slot />
+<div class="bg-first w-screen h-screen">
+	<slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,26 @@
-<h1 class="bg-slate-500">Welcome to Svelte</h1>
-<p class="">
-	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<script lang="ts">
+	import HeaderPhoto from '$lib/HeaderPhoto.svelte';
+</script>
+
+<div class="flex flex-row bg-second px-5 py-10 gap-32 items-center">
+	<div class="flex flex-row md:flex-col">
+		<div class="flex flex-row">
+			<HeaderPhoto />
+			<div class="flex flex-col">
+				<h1 class=" text-first pl-1">Justus</h1>
+				<h1 class=" text-first pl-5 italic tracking-tight">Kilian</h1>
+			</div>
+		</div>
+		<h1 class=" text-first pl-12 py-0 font-bold tracking-wide">Wolff</h1>
+		Buttons
+	</div>
+	<p class="text-first">
+		Welcome to Max's corner, where Hamburg meets machine learning! As a student from this vibrant
+		city, Max is passionate about unraveling the mysteries of AI. Join him on his journey through
+		the world of algorithms and data analysis. Let's explore together the fascinating realm where
+		Hamburg's charm intersects with cutting-edge technology. Let's get in touch :)
+	</p>
+</div>
+<div class="px-5 py-10">
+	<h2 class="text-second">Projects</h2>
+</div>
