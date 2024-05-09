aea2d07814405d5f9a0a8ac6bf1e082126d99247
justuskilianwolff <justus.wolff@web.de>
2024-05-09 23:14:34 +0200

    photo

--- src/lib/HeaderPhoto.svelte
+++ src/lib/HeaderPhoto.svelte
@@ -2,6 +2,6 @@
 	import img from '$lib/assets/example.jpg';
 </script>
 
-<div class="flex justify-center items-center h-48 w-48 rounded-full float-start">
+<div class="flex justify-center items-center h-32 w-32 md:h-48 md:w-48 rounded-full float-start">
 	<img class="rounded-full h-full w-full object-cover" src={img} alt="Headshot" />
 </div>
