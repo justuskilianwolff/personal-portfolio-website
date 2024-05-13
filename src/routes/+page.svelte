79d43e9793dd6916ee5b64efb7e3caec6513928c
justuskilianwolff <justus.wolff@web.de>
2024-05-13 21:52:51 +0200

    something

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,46 +4,60 @@
 	import Section from '$lib/Section.svelte';
 	import Card from '$lib/Card.svelte';
 	import Masonry from '$lib/Masonry.svelte';
+	import SectionHeader from '$lib/SectionHeader.svelte';
 </script>
 
-<Section backgroundColor="second">
-	<div class="bg-second px-5 pt-10">
-		<div class="flex flex-col gap-4 justify-between md:flex-row md:items-center md:gap-0">
-			<div class="flex flex-col">
-				<div class="flex flex-row">
-					<HeaderPhoto />
-					<div class="flex flex-col">
-						<h1 class=" text-first pl-1">Justus</h1>
-						<h1 class=" text-first pl-5 italic tracking-tight">Kilian</h1>
-					</div>
+<Section class="bg-second py-8">
+	<div class="flex flex-col gap-4 justify-between md:flex-row md:items-center md:gap-0">
+		<div class="flex flex-col">
+			<div class="flex flex-row">
+				<HeaderPhoto />
+				<div class="flex flex-col">
+					<h1 class=" text-first pl-1">Justus</h1>
+					<h1 class=" text-first pl-5 italic tracking-tight">Kilian</h1>
 				</div>
-				<h1 class=" text-first pl-12 py-0 font-bold tracking-wide">Wolff</h1>
 			</div>
-			<p class="text-first px-4 max-w-80 md:text-right">
-				Welcome to Max's corner, where Hamburg meets machine learning! As a student from this
-				vibrant city, Max is passionate about unraveling the mysteries of AI. Join him on his
-				journey through the world of algorithms and data analysis. Let's explore together the
-				fascinating realm where Hamburg's charm intersects with cutting-edge technology. Let's get
-				in touch :)
-			</p>
+			<h1 class=" text-first pl-12 py-0 font-bold tracking-wide">Wolff</h1>
 		</div>
+		<p class="text-first px-4 max-w-80 md:text-right">
+			Welcome to Max's corner, where Hamburg meets machine learning! As a student from this vibrant
+			city, Max is passionate about unraveling the mysteries of AI. Join him on his journey through
+			the world of algorithms and data analysis. Let's explore together the fascinating realm where
+			Hamburg's charm intersects with cutting-edge technology. Let's get in touch :)
+		</p>
 	</div>
 </Section>
-<Section backgroundColor="first" class="bg-first pb-12">
-	<div class="px-5 py-10">
-		<h2 class="text-second">Projects</h2>
-	</div>
+<Section class="bg-first">
+	<SectionHeader heading="Projects" class="text-second" />
 	<Masonry>
+		<Card title="MOPTA" description="desc" content="content" footer="footer" />
+		<Card title="MOPTA" description="desc" content="content" footer="footer" />
+		<Card title="MOPTA" description="desc" content="content" footer="footer" />
+		<Card title="MOPTA" description="desc" content="content" footer="footer" />
 		<Card title="MOPTA" description="desc" content="content" footer="footer" />
 		<Card title="MOPTA" description="desc" content="content" footer="footer" />
 		<Card title="MOPTA" description="desc" content="content" footer="footer" />
 		<Card title="MOPTA" description="desc" content="content" footer="footer" />
 	</Masonry>
 </Section>
-
-<div class=" pt-10 pb-4 flex flex-row justify-center gap-10 items-center">
-	<a href="mailto:moin@justuskilianwolff.io">moin@justuskilianwolff.io</a>
-	<iconify-icon icon="mdi:github" />
-	<iconify-icon icon="mdi:linkedin" />
-	<iconify-icon icon="mdi:instagram" />
-</div>
+<Section class="bg-second">
+	<SectionHeader heading="Work" class="text-first" />
+	<Masonry>
+		<Card title="MOPTA" description="desc" content="content" footer="footer" />
+		<Card title="MOPTA" description="desc" content="content" footer="footer" />
+		<Card title="MOPTA" description="desc" content="content" footer="footer" />
+		<Card title="MOPTA" description="desc" content="content" footer="footer" />
+		<Card title="MOPTA" description="desc" content="content" footer="footer" />
+		<Card title="MOPTA" description="desc" content="content" footer="footer" />
+		<Card title="MOPTA" description="desc" content="content" footer="footer" />
+		<Card title="MOPTA" description="desc" content="content" footer="footer" />
+	</Masonry>
+</Section>
+<Section class="bg-second">
+	<div class=" pt-10 pb-4 flex flex-row justify-center gap-10 items-center">
+		<a href="mailto:moin@justuskilianwolff.io">moin@justuskilianwolff.io</a>
+		<iconify-icon icon="mdi:github" />
+		<iconify-icon icon="mdi:linkedin" />
+		<iconify-icon icon="mdi:instagram" />
+	</div>
+</Section>
