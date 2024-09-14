9dc5168c0485410509dc8aa7597c4e671bdf0a02
Justus Kilian Wolff <justus.wolff@web.de>
2024-09-14 13:01:56 +0200

    updates

--- backend/cms/admin.py
+++ backend/cms/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from solo.admin import SingletonModelAdmin
 
-from .models import IconLink, Link, PersonalInfo
+from .models import IconLink, Milestone, PersonalInfo, Project, TextLink, Work
 
 admin.site.register(PersonalInfo, SingletonModelAdmin)
 admin.site.register(IconLink)
-admin.site.register(Link)
+admin.site.register(TextLink)
+admin.site.register(Project)
+admin.site.register(Milestone)
+admin.site.register(Work)
--- backend/cms/models.py
+++ backend/cms/models.py
@@ -1,16 +1,6 @@
 from django.db import models
 from solo.models import SingletonModel
 
-# type Project = {
-# 	title: Translation;
-# 	subtitle: Translation;
-# 	description: Translation;
-# 	technologies: Technology[];
-# 	links: Link[];
-# };
-
-# Projects, Work, milestones (CV)
-
 
 class PersonalInfo(SingletonModel):
     name = models.CharField(max_length=100)
@@ -30,14 +20,49 @@ class Link(models.Model):
     name = models.CharField(max_length=100)
 
     class Meta:
-        abstract = False
+        abstract = True
 
-    def __str__(self):
-        return self.name
+
+class TextLink(Link):
+    class Meta:
+        verbose_name = "Text Link"
+        verbose_name_plural = "Text Links"
 
 
 class IconLink(Link):
     iconify_logo = models.CharField(max_length=100)
 
     class Meta:
+        verbose_name = "Icon Link"
         verbose_name_plural = "Icon Links"
+
+
+class Milestone(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    date = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+
+class Activity(models.Model):
+    title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=100)
+    description = models.TextField()
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+
+class Project(Activity):
+    technologies = models.ManyToManyField("IconLink", related_name="projects_technologies")
+    links = models.ManyToManyField("TextLink", related_name="projects_links")
+
+
+class Work(Activity):
+    technologies = models.ManyToManyField("IconLink", related_name="works_technologies")
+    links = models.ManyToManyField("TextLink", related_name="works_links")
