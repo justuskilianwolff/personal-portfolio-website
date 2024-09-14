2fb4d48a7257bfe36be29a7905a3ced1a7d3f88c
Justus Kilian Wolff <justus.wolff@web.de>
2024-09-14 11:52:01 +0200

    added link

--- backend/cms/admin.py
+++ backend/cms/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from solo.admin import SingletonModelAdmin
 
-from .models import IconLink, PersonalInfo
+from .models import IconLink, Link, PersonalInfo
 
 admin.site.register(PersonalInfo, SingletonModelAdmin)
 admin.site.register(IconLink)
+admin.site.register(Link)
