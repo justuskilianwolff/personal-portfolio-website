ad6596f0ef8c0600ca6435da0106ca72b96ba70e
Justus Kilian Wolff <justus.wolff@web.de>
2024-09-14 10:57:22 +0200

    too stupid to do migrations (give me some more hours with django)

--- backend/cms/models.py
+++ backend/cms/models.py
@@ -30,21 +30,16 @@ class PersonalInfo(SingletonModel):
         return self.name
 
 
-class IconLink(models.Model):
-    name = models.CharField(max_length=100)
-    iconify_logo = models.CharField(max_length=100)
-    link = models.URLField()
-
-    class Meta:
-        verbose_name_plural = "Icon Links"
-
-    def __str__(self):
-        return self.name
-
-
 class Link(models.Model):
     url = models.URLField()
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
+
+
+class IconLink(Link):
+    iconify_logo = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = "Icon Links"
