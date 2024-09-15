387518d18ab7ceda607d7d852c95a9153c1d3fb3
Justus Kilian Wolff <justus.wolff@web.de>
2024-09-15 21:03:23 +0200

    added views

--- backend/cms/views.py
+++ backend/cms/views.py
@@ -1,17 +1,23 @@
-from django.http import HttpRequest
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-
-from cms.models import PersonalInfo
-from cms.serializers import ProductSerializer
-
-# FIXME
+from cms.models import Milestone, PersonalInfo, Project, Work
+from cms.serializers import MilestoneSerializer, PersonalInfoSerializer, ProjectSerializer, WorkSerializer
+from rest_framework import viewsets
 
 
-@api_view(["GET"])
-def api_home(request: HttpRequest, filter: str | None = None, *args, **kwargs):
-    products = PersonalInfo.objects.all()
-    products_list = []
-    for product in products:
-        products_list.append(ProductSerializer(product).data)
-    return Response(products_list)
+class PersonalInfoViewSet(viewsets.ModelViewSet):
+    queryset = PersonalInfo.objects.all()
+    serializer_class = PersonalInfoSerializer
+
+
+class MilestoneViewSet(viewsets.ModelViewSet):
+    queryset = Milestone.objects.all()
+    serializer_class = MilestoneSerializer
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+
+class WorkViewSet(viewsets.ModelViewSet):
+    queryset = Work.objects.all()
+    serializer_class = WorkSerializer
