2d99d38a1623135385e974e5d7f0a9dc88d9747c
Justus Kilian Wolff <justus.wolff@web.de>
2024-09-15 21:03:39 +0200

    added serializers

--- backend/cms/serializers.py
+++ backend/cms/serializers.py
@@ -1,11 +1,51 @@
+from cms.models import Activity, IconLink, Milestone, PersonalInfo, Project, TextLink, Work
 from rest_framework import serializers
 
-from cms.models import PersonalInfo
 
-# TODO: fix
+class IconLinkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = IconLink
+        fields = "__all__"
 
 
-class ProductSerializer(serializers.ModelSerializer):
+class PersonalInfoSerializer(serializers.ModelSerializer):
+    links = IconLinkSerializer(many=True, read_only=True)
+
     class Meta:
         model = PersonalInfo
-        fields = ["id", "title", "description", "price", "get_discounted_price"]
+        fields = "__all__"
+
+
+class TextLinkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TextLink
+        fields = "__all__"
+
+
+class MilestoneSerializer(serializers.ModelSerializer):
+    # TODO: sort by date
+
+    class Meta:
+        model = Milestone
+        fields = "__all__"
+
+
+class ActivitySerializer(serializers.ModelSerializer):
+    technologies = IconLinkSerializer(many=True, read_only=True)
+    links = TextLinkSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Activity
+        fields = "__all__"
+
+
+class WorkSerializer(ActivitySerializer):
+    class Meta:
+        model = Work
+        fields = "__all__"
+
+
+class ProjectSerializer(ActivitySerializer):
+    class Meta:
+        model = Project
+        fields = "__all__"
