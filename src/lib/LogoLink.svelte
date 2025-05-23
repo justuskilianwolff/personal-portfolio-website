4eeba48ea9fb4a0b0674497584b549f75b646978
Justus Kilian Wolff <justus.wolff@web.de>
2025-05-23 22:58:04 +0200

    created a logolink class

---
+++ src/lib/LogoLink.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { Linkedin, Github, Twitter, Mail } from '@lucide/svelte';
+
+  let { text, url, icon }: { text: string; url: string; icon?: string } = $props();
+
+  const iconMap = {
+    linkedin: Linkedin,
+    github: Github,
+    twitter: Twitter,
+    mail: Mail
+  };
+
+  const IconComponent = $derived(icon && iconMap[icon as keyof typeof iconMap] ? iconMap[icon as keyof typeof iconMap] : null);
+</script>
+
+<a href={url} class="badge border-inherit badge-md gap-2">
+  {#if IconComponent}
+    <IconComponent size={16} />
+  {/if}
+  {text}
+</a>
+
