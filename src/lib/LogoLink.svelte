24814ccd46176d9f187c8813eca4648d727ec3cb
Justus Kilian Wolff <justus.wolff@web.de>
2025-06-02 23:03:32 +0200

    merged

--- src/lib/LogoLink.svelte
+++ src/lib/LogoLink.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { Github, Instagram, Linkedin, Loader, Mail } from '@lucide/svelte';
 
-	let { url, name: icon }: { url: string; name: string } = $props();
+	let { url, name: icon, text }: { url: string; name: string; text?: string } = $props();
 
 	const iconMap = {
 		linkedin: Linkedin,
@@ -10,9 +10,14 @@
 		mail: Mail
 	};
 
-	const IconComponent = icon in iconMap ? iconMap[icon as keyof typeof iconMap] : Loader;
+	const IconComponent = $derived(
+		icon && iconMap[icon as keyof typeof iconMap] ? iconMap[icon as keyof typeof iconMap] : Loader
+	);
 </script>
 
-<a href={url} class="badge badge-md gap-2 rounded-full">
+<a href={url} class="badge badge-md gap-2 border-inherit">
 	<IconComponent size={16} />
+	{#if text}
+		{text}
+	{/if}
 </a>
