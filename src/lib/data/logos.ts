b8ca1a47ecf06c151a244025d27d916df143b754
justuskilianwolff <justus.wolff@web.de>
2025-06-10 20:42:42 +0200

    added some logog

--- src/lib/data/logos.ts
+++ src/lib/data/logos.ts
@@ -10,3 +10,38 @@ export const CPLEX: Icon = {
 	hint: 'CPLEX',
 	url: 'https://www.ibm.com/products/ilog-cplex-optimization-studio'
 };
+export const LaTeX: Icon = {
+	identifier: 'latex',
+	hint: 'LaTeX',
+	url: 'https://www.latex-project.org/'
+};
+export const R: Icon = {
+	identifier: 'r',
+	hint: 'R',
+	url: 'https://www.r-project.org/'
+};
+export const TensorFlow: Icon = {
+	identifier: 'tensorflow',
+	hint: 'TensorFlow',
+	url: 'https://www.tensorflow.org/'
+};
+export const Svelte: Icon = { 
+	identifier: 'svelte',
+	hint: 'Svelte',
+	url: 'https://svelte.dev/'
+};
+export const Git: Icon = {
+	identifier: 'git',
+	hint: 'Git',
+	url: 'https://git-scm.com/'
+};
+export const Docker: Icon = {
+	identifier: 'docker',
+	hint: 'Docker',
+	url: 'https://www.docker.com/'
+};
+export const VSCode: Icon = {
+	identifier: 'vscode',
+	hint: 'VS Code',
+	url: 'https://code.visualstudio.com/'
+};
