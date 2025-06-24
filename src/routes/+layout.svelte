545983524308640f71686aad7b510d2b89cc1032
justuskilianwolff <justus.wolff@web.de>
2025-06-24 17:21:05 +0200

    tab header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,10 @@
 	let { children } = $props();
 </script>
 
+<svelte:head>
+	<title>Justus Kilian Wolff</title>
+</svelte:head>
+
 <div class="m-5 mx-auto min-h-screen w-full max-w-4xl px-4 pt-16">
 	{@render children()}
 </div>
