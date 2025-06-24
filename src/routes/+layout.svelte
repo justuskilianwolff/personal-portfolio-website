25445c6a161f13505a2fea4e0656c75493e22d33
justuskilianwolff <justus.wolff@web.de>
2025-06-24 17:15:41 +0200

    fixed jumping layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,8 @@
 	import '../app.css';
 
 	let { children } = $props();
-	
-
 </script>
 
-<div class="flex min-h-screen items-center justify-center">
-	<div class="m-5 mx-auto px-4 md:max-w-8/10 lg:max-w-7/10 2xl:max-w-6/10">
-		{@render children()}
-	</div>
+<div class="m-5 mx-auto min-h-screen w-full max-w-4xl px-4 pt-16">
+	{@render children()}
 </div>
